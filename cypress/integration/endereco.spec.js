f6fee477bb66ec4ec8d9e3daddd548d452650df2
Helton Imroth <heltonimroth@gmail.com>
2022-01-11 22:17:54 -0300

    page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -12,7 +13,9 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     //Teste com comando customizados para login(adicionado beforeEach) e cadastro de endereço dentro da pasta support no arquivo commands
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-
+    //Teste usando paga objects, arquivo endereco.page.js    
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Helton', 'Imroth', 'Empresaaaa', 'Brasil', 'Ederecoooo', '2321', 'São Paulo', 'São Paulo', '89053210', '312321', 'email@email.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
     });
 });
